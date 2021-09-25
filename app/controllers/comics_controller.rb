4042bfa8c6bc6e89d3453f22dcad60a8ded18b7a
Hugo Tamaki <hugokt2005@yahoo.com.br>
2021-09-25 20:42:05 -0300

    Getting distinct comics only

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,6 +1,6 @@
 class ComicsController < ApplicationController
   def index
-    comics = Comic.order(sold_at: :desc)
+    comics = Comic.distinct.order(sold_at: :desc)
     comics = comics.joins(:characters).where('LOWER(characters.name) LIKE ?', "%#{params[:search].downcase}%") if params[:search].present?
 
     @comics = comics.page(params[:page] || 1).per(15)
